df9d2e2cc16e5408acadfd305e3ceccbd5a30291
kakit <707828130@qq.com>
2016-10-20 15:36:16 +0800

    Update 48.cpp

--- 48.cpp
+++ 48.cpp
@@ -3,7 +3,7 @@ public:
     void rotate(vector<vector<int> >& matrix) {
     	int n = matrix.size(), l = 0, r = n-1;
     	if(n <= 1) return;
-        for(int i=0; i<n-1; ++i)
+        for(int i=0; i<n/2; ++i)
         {
         	for(int j=l; j<r; ++j)
         	{
